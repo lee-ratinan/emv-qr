7ee7a09df2cf5182ab4e3591b05bad46bf6d42ef
Ratinan Lee <lee.ratinan@gmail.com>
2021-09-18 15:30:59 +0800

    Add generator and qr code scanner for test folder

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
-<html>
-  <head>
-    <title>EmvMerchant</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col col-md-8 col-lg-6">
-          <a href="test/decode.php">Decoder Test</a>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
+<?php
+header('Location: test/decoder.php');
