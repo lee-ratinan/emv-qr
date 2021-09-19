f47ba1b6e47950cac744ec6d7f2972b125ee19e0
Ratinan Lee <lee.ratinan@gmail.com>
2021-09-19 17:25:43 +0800

    add meta

--- test/decoder-scan.html
+++ test/decoder-scan.html
@@ -9,6 +9,10 @@ if ( ! empty($str))
 ?>
 <html>
   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="emvco qr code">
+    <meta name="author" content="Ratinan Lee">
     <title>TEST DECODER</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
--- test/generator.php
+++ test/generator.php
@@ -80,6 +80,10 @@ function print_status($status)
 ?>
 <html>
   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="emvco qr code">
+    <meta name="author" content="Ratinan Lee">
     <title>TEST GENERATOR</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
